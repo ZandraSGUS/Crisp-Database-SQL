fc16cec28f56fc4c3c0e1f0a5918e0d2541b6536
ZandraSGUS <zandra.g.chen@gmail.com>
2021-07-05 03:23:15 +0800

    Tested to confirm code is working

--- category.sql
+++ category.sql
@@ -1,6 +1,6 @@
 create table category (
     description_id VARCHAR(50),
-    category VARCHAR(50),
+    category VARCHAR(50)
 );
 insert into category (description_id, category) values ('GV', 'ENTERTAINMENT');
 insert into category (description_id, category) values ('BUS', 'TRANSPORT');
--- transactions.sql
+++ transactions.sql
@@ -3,20 +3,20 @@ create table transactions (
     amount DECIMAL(10,2),
     transaction_date DATE,
     description_id VARCHAR(50),
-    bank_account_id VARCHAR(50),
+    bank_account_id VARCHAR(50)
 );
-insert into transactions (transaction_id, amount, date, description_id, bank_account_id) values (1, 3.70, '14/04/2021', 'GIFTS', '03093658CQ');
-insert into transactions (transaction_id, amount, date, description_id, bank_account_id) values (2, 2.10, '19/05/2021', 'MRT', '86826321VV');
-insert into transactions (transaction_id, amount, date, description_id, bank_account_id) values (3, 9.00, '05/04/2021', 'ELECTRONICS', '86826321VV');
-insert into transactions (transaction_id, amount, date, description_id, bank_account_id) values (4, 5.45, '27/03/2021', 'NTUC', '31324841WN');
-insert into transactions (transaction_id, amount, date, description_id, bank_account_id) values (5, 2.20, '19/06/2021', 'MRT', '03093658CQ');
-insert into transactions (transaction_id, amount, date, description_id, bank_account_id) values (6, 0.75, '03/06/2021', 'BUS', '03093658CQ');
-insert into transactions (transaction_id, amount, date, description_id, bank_account_id) values (7, 19.30, '11/05/2021', 'REDMART', '86826321VV');
-insert into transactions (transaction_id, amount, date, description_id, bank_account_id) values (8, 4.45, '08/04/2021', 'NTUC', '86826321VV');
-insert into transactions (transaction_id, amount, date, description_id, bank_account_id) values (9, 6.50, '31/03/2021', 'GV', '86826321VV');
-insert into transactions (transaction_id, amount, date, description_id, bank_account_id) values (10, 6.55, '28/06/2021', 'GIFTS', '03093658CQ');
-insert into transactions (transaction_id, amount, date, description_id, bank_account_id) values (11, 13.75, '16/03/2021', 'GRAB', '86826321VV');
-insert into transactions (transaction_id, amount, date, description_id, bank_account_id) values (12, 4.50, '15/05/2021', 'ELECTRONICS', '31324841WN');
-insert into transactions (transaction_id, amount, date, description_id, bank_account_id) values (13, 6.55, '11/05/2021', 'FOOD', '31324841WN');
-insert into transactions (transaction_id, amount, date, description_id, bank_account_id) values (14, 9.20, '27/06/2021', 'GIFTS', '31324841WN');
-insert into transactions (transaction_id, amount, date, description_id, bank_account_id) values (15, 8.65, '21/04/2021', 'NTUC', '03093658CQ');
+insert into transactions (transaction_id, amount, transaction_date, description_id, bank_account_id) values (1, 3.70, '2021-04-14', 'GIFTS', '03093658CQ');
+insert into transactions (transaction_id, amount, transaction_date, description_id, bank_account_id) values (2, 2.10, '2021-05-19', 'MRT', '86826321VV');
+insert into transactions (transaction_id, amount, transaction_date, description_id, bank_account_id) values (3, 9.00, '2021-04-05', 'ELECTRONICS', '86826321VV');
+insert into transactions (transaction_id, amount, transaction_date, description_id, bank_account_id) values (4, 5.45, '2021-03-27', 'NTUC', '31324841WN');
+insert into transactions (transaction_id, amount, transaction_date, description_id, bank_account_id) values (5, 2.20, '2021-06-19', 'MRT', '03093658CQ');
+insert into transactions (transaction_id, amount, transaction_date, description_id, bank_account_id) values (6, 0.75, '2021-06-03', 'BUS', '03093658CQ');
+insert into transactions (transaction_id, amount, transaction_date, description_id, bank_account_id) values (7, 19.30, '2021-05-11', 'REDMART', '86826321VV');
+insert into transactions (transaction_id, amount, transaction_date, description_id, bank_account_id) values (8, 4.45, '2021-04-08', 'NTUC', '86826321VV');
+insert into transactions (transaction_id, amount, transaction_date, description_id, bank_account_id) values (9, 6.50, '2021-03-31', 'GV', '86826321VV');
+insert into transactions (transaction_id, amount, transaction_date, description_id, bank_account_id) values (10, 6.55, '2021-06-28', 'GIFTS', '03093658CQ');
+insert into transactions (transaction_id, amount, transaction_date, description_id, bank_account_id) values (11, 13.75, '2021-03-16', 'GRAB', '86826321VV');
+insert into transactions (transaction_id, amount, transaction_date, description_id, bank_account_id) values (12, 4.50, '2021-05-15', 'ELECTRONICS', '31324841WN');
+insert into transactions (transaction_id, amount, transaction_date, description_id, bank_account_id) values (13, 6.55, '2021-05-11', 'FOOD', '31324841WN');
+insert into transactions (transaction_id, amount, transaction_date, description_id, bank_account_id) values (14, 9.20, '2021-06-27', 'GIFTS', '31324841WN');
+insert into transactions (transaction_id, amount, transaction_date, description_id, bank_account_id) values (15, 8.65, '2021-04-21', 'NTUC', '03093658CQ');
